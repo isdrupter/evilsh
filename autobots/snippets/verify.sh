d3dee312cd1ca4d772c2ac909041e13783c609a4
Shellz/KekSec <isdrupter@users.noreply.github.com>
2016-10-29 06:52:34 -0400

    Create verify.sh

---
+++ autobots/snippets/verify.sh
@@ -0,0 +1,6 @@
+
+# posix compliant - not all embedded echo binaries have the -e option, 
+# and not all embedded systems have printf, but I think all unix systems
+# have /etc/passwd and cat present, so this should be a relatively 
+# foolproof method to determine if a system is real or not.
+if ([ `/bin/echo -e "\\x6c\\x6f\\x6c"` == "lsol" ] || [ `printf "\\x6c\\x6f\\x6c"` == "lsol" ] || /bin/echo `cat /etc/passwd` >/dev/null 2>&1 );then echo g0t0ne ;fi
